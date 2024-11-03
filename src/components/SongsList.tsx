770be2155bb72aa8ae15745466ccd43adf942957
Mashood ur Rehman Rana <ranamashood@proton.me>
2024-11-03 18:58:58 +0500

    style: scrollbar styling

--- src/components/SongsList.tsx
+++ src/components/SongsList.tsx
@@ -44,6 +44,41 @@ const Container = styled.div`
   flex-direction: column;
   overflow-y: auto;
   gap: 5px;
+  padding-right: 10px;
+
+  &::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px #292733;
+    border-radius: 12px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #6d4aff;
+    border-radius: 12px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #5c33ff;
+  }
+
+  &::-webkit-scrollbar-thumb:active {
+    background-color: #8566ff;
+  }
+
+  @-moz-document url-prefix() {
+    scrollbar-color: #6d4aff #292733;
+
+    &:hover {
+      scrollbar-color: #5c33ff #292733;
+    }
+
+    &:active {
+      scrollbar-color: #8566ff #292733;
+    }
+  }
 `;
 
 const Button = styled.button<{ active: boolean }>`
